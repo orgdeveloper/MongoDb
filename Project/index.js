36ae273deec46cdaf3b18cf40e2ab36e41363391
Help <pczerokey@gmail.com>
2019-08-06 17:43:21 -0400

    Accessing snowboard data
    took a long time to figure this out, as some things were changed in the npm module had to do shit tons of search to figure this out.

--- Project/index.js
+++ Project/index.js
@@ -1,9 +1,29 @@
 const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
-
 const url = 'mongodb://localhost:27017/learning_mongo';
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'learning_mongo';
+
+
+const findDocuments = (db, callback) => {
+    const collection = db.collection('tours');
+
+    collection.find({"tourPackage":"Snowboard Cali"}).toArray( (error, docs) => {
+        console.log(docs);
+        callback();
+    })
+};
+
+
 MongoClient.connect(url, { useNewUrlParser: true } , (err, db) => {
+    if (err) return process.exit(1);
     console.log('Connected to your local hosted MongoDb Server!');
-    db.close();
+
+    const database = db.db('learning_mongo');
+
+    findDocuments(database, () => {
+        db.close();
+    })
 });
+
